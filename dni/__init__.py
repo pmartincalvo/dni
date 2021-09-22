4c2cd76eb0c16fce2e4c04e92ecb84dfeeab09f1
pablo <pablomartincalvo@gmail.com>
2021-09-22 20:35:46 +0200

    Modified to check pylint.

--- dni/__init__.py
+++ dni/__init__.py
@@ -1,5 +1,8 @@
 def compute_checksum(dni_number: str) -> str:
+    """
+    Given a DNI number, obtain the correct checksum.
+    :param dni_number: a valid dni number.
+    :return: the checksum for the number as an uppercase, single character
+    string.
+    """
     return "TRWAGMYFPDXBNJZSQVHLCKE"[int(dni_number) % 23]
-
-def add_checksum(dni_number: str) -> str:
-    return dni_number+compute_checksum(dni_number)
